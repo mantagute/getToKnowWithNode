36a51b9823572bbbd7c5befa2f5e04e38841f164
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-02-19 22:18:58 -0300

    getting how express can make our lives MUCH easier.

--- expressFundamentals/app.js
+++ expressFundamentals/app.js
@@ -1,24 +1,20 @@
-const http = require('http');
-const {readFileSync} = require('fs');
+const express = require('express');
 
-const homePage = readFileSync('./index.html')
+const app = express()
 
-const server = http.createServer((request, response) => {
-    const url = request.url
-
-    if (url === '/') {
-        response.writeHead(200, {'content-type':'text/html'})
-        response.end(homePage)
-    }
-    else if (url === '/about') {
-        response.writeHead(200, {'content-type':'text/html'})
-        response.end('<h1>About page</h1>')
-    }
-    else {
-        response.writeHead(404, {'content-type':'text/html'})
-        response.end('<h1>Page not found</h1>')
-    }
-
+app.get('/',(request, response) => {
+    console.log('user hit the resource')
+    response.status(200).send('Home Page')
 })
 
-server.listen(8000)
+app.get('/about', (request, response) => {
+    response.status(200).send('About Page')
+})
+
+app.use((request, response) => {
+    response.status(404).send('<h1>Resource Not Found</h1>')
+})
+
+app.listen(8000, () => {
+    console.log('server is listening on port 8000...')
+})
