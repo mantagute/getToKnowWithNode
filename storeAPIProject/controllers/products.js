f6d05c2caf7ad1e5a1565c66eb1ff1ce436995e5
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-04-11 23:00:54 -0300

    feat: get all produtcs that match... name/company/featured

--- storeAPIProject/controllers/products.js
+++ storeAPIProject/controllers/products.js
@@ -1,9 +1,35 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (request, response) => {
-    response.status(200).json({message: "products testing route"});
+
+
+    const products = await Product.find({
+        name: '2',
+    });
+    response.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (request, response) => {
-    response.status(200).json({message: "products route"});
+    const {featured, company, name } = request.query; 
+    
+    queryObject = {};
+
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+
+    if (company) {
+        queryObject.company = company;
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i'};
+    }
+
+    console.log(queryObject);
+
+    const products = await Product.find(queryObject);
+    response.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {
